f9cfd297186f704a78f9eda19ccbb0aa6fb1b86e
harshdeep2021 <tocontactharshdeepkaur@gmail.com>
2022-11-26 19:58:29 -0500

    by harshdeep

---
+++ ui/Notes/__init__.py
@@ -0,0 +1,26 @@
+from _ast import Delete
+from typing import Type
+
+def create_notes(noteTitle=None):
+    ourNote = Type.Note()
+    ourNote.title = noteTitle
+
+
+class Edit:
+    pass
+
+def Edit_notes(noteTitle=None):
+    ourNote = Edit.Notes()
+    ourNote.title= noteTitle
+    
+def save_notes(noteTitle=None, save=None):
+    ourNote = save.Note()
+    ourNote.title = noteTitle
+
+def Delete_notes(noteTitle=None):
+    ourNote = Delete.Note()
+    ourNote.title = noteTitle
+
+def SaveToDatabase():
+    conn.database =
+    
